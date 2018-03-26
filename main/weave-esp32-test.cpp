c2830b7bc1ab95f0c6d5ff0d2c3da1dd2f066e53
Jay Logue <jaylogue@google.com>
2018-03-25 21:07:49 -0700

    Project Blue Sky : Greatly simplified test device code.

--- main/weave-esp32-test.cpp
+++ main/weave-esp32-test.cpp
@@ -1,90 +1,18 @@
-/* UDP MultiCast Send/Receive Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-#include <string.h>
-#include <sys/param.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "freertos/queue.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event_loop.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
 
-#include "lwip/err.h"
-#include "lwip/sys.h"
-#include "lwip/tcpip.h"
-#include "arch/sys_arch.h"
-#include <lwip/netdb.h>
-
 #include <WeavePlatform-ESP32.h>
-#include <InetLayer/InetLayer.h>
-#include <InetLayer/IPAddress.h>
-#include <Weave/Support/logging/WeaveLogging.h>
 #include <Weave/Support/ErrorStr.h>
-#include <InetLayer/UDPEndPoint.h>
 
-using namespace nl;
-using namespace nl::Inet;
-using namespace nl::Weave;
-
-/* The examples use simple WiFi configuration that you can set via
-   'make menuconfig'.
-
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-#define EXAMPLE_WIFI_SSID CONFIG_WIFI_SSID
-#define EXAMPLE_WIFI_PASS CONFIG_WIFI_PASSWORD
+using namespace ::nl;
+using namespace ::nl::Inet;
+using namespace ::nl::Weave;
+using namespace ::WeavePlatform;
 
 static const char *TAG = "weave-esp32-test";
-
-void DumpWiFiState()
-{
-    WEAVE_ERROR err;
-    wifi_mode_t wifiMode;
-    wifi_config_t wifi_config;
-    bool autoConnect;
-
-    err = esp_wifi_get_mode(&wifiMode);
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "esp_wifi_get_mode return error: %d", err);
-    }
-    else
-    {
-        ESP_LOGI(TAG, "wifiMode is %d", wifiMode);
-    }
-
-    memset(&wifi_config, 0, sizeof(wifi_config));
-    err = esp_wifi_get_config(ESP_IF_WIFI_STA, &wifi_config);
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "esp_wifi_get_config return error: %d", err);
-    }
-    else
-    {
-        ESP_LOGI(TAG, "wifi_config.sta.ssid is '%s'", wifi_config.sta.ssid);
-        ESP_LOGI(TAG, "wifi_config.sta.password is '%s'", wifi_config.sta.password);
-    }
-
-    err = esp_wifi_get_auto_connect(&autoConnect);
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "esp_wifi_get_auto_connect return error: %d", err);
-    }
-    else
-    {
-        ESP_LOGI(TAG, "autoConnect is '%s'", (autoConnect) ? "true" : "false");
-    }
-}
 
 enum
 {
@@ -97,25 +25,12 @@ void HandleAliveTimer(System::Layer * aLayer, void * aAppState, System::Error aE
 
     ESP_LOGI(TAG, "Alive");
 
-    err = WeavePlatform::SystemLayer.StartTimer(kAliveInterval, HandleAliveTimer, NULL);
+    err = SystemLayer.StartTimer(kAliveInterval, HandleAliveTimer, NULL);
     if (err != WEAVE_NO_ERROR)
     {
         ESP_LOGE(TAG, "Failed to start timer: %s", ErrorStr(err));
         return;
     }
-
-#if 0
-    if (WeavePlatform::SystemLayer.GetSystemTimeMS() > 15000)
-    {
-        static bool done = false;
-
-        if (!done)
-        {
-            ::WeavePlatform::ConnectivityMgr.DemandStartWiFiAP();
-            done = true;
-        }
-    }
-#endif
 }
 
 extern "C" void app_main()
@@ -124,13 +39,13 @@ extern "C" void app_main()
 
     ESP_ERROR_CHECK( nvs_flash_init() );
 
-    if (!WeavePlatform::InitLwIPCoreLock()) {
+    if (!InitLwIPCoreLock()) {
         return;
     }
 
     tcpip_adapter_init();
 
-    ESP_ERROR_CHECK( esp_event_loop_init(WeavePlatform::HandleESPSystemEvent, NULL) );
+    ESP_ERROR_CHECK( esp_event_loop_init(HandleESPSystemEvent, NULL) );
 
     {
         wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -138,36 +53,19 @@ extern "C" void app_main()
 
         ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
 
-        ESP_LOGI(TAG, "esp_wifi_set_mode(WIFI_MODE_STA)");
-        ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-
-        wifi_config_t wifiConfig;
-        memset(&wifiConfig, 0, sizeof(wifiConfig));
-        memcpy(wifiConfig.sta.ssid, EXAMPLE_WIFI_SSID, strlen(EXAMPLE_WIFI_SSID) + 1);
-        memcpy(wifiConfig.sta.password, EXAMPLE_WIFI_PASS, strlen(EXAMPLE_WIFI_PASS) + 1);
-        wifiConfig.sta.scan_method = WIFI_ALL_CHANNEL_SCAN;
-        wifiConfig.sta.sort_method = WIFI_CONNECT_AP_BY_SIGNAL;
-        ESP_LOGI(TAG, "esp_wifi_set_config(ESP_IF_WIFI_STA, &wifiConfig)");
-        ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifiConfig) );
-
-        ESP_LOGI(TAG, "esp_wifi_set_auto_connect(true)");
-        ESP_ERROR_CHECK( esp_wifi_set_auto_connect(true) );
-
-        ESP_LOGI(TAG, "esp_wifi_set_mode(WIFI_MODE_NULL)");
         ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
 
-        ESP_LOGI(TAG, "esp_wifi_start()");
         ESP_ERROR_CHECK( esp_wifi_start() );
     }
 
-    if (!WeavePlatform::InitWeaveStack())
+    if (!InitWeaveStack())
     {
         return;
     }
 
-    ::WeavePlatform::ConnectivityMgr.SetWiFiAPMode(WeavePlatform::ConnectivityManager::kWiFiAPMode_Enabled);
+    ConnectivityMgr.SetWiFiAPMode(ConnectivityManager::kWiFiAPMode_OnDemand_NoStationProvision);
 
-    err = WeavePlatform::SystemLayer.StartTimer(kAliveInterval, HandleAliveTimer, NULL);
+    err = SystemLayer.StartTimer(kAliveInterval, HandleAliveTimer, NULL);
     if (err != WEAVE_NO_ERROR) {
         ESP_LOGE(TAG, "Failed to start timer: %s", ErrorStr(err));
         return;
@@ -175,5 +73,5 @@ extern "C" void app_main()
 
     ESP_LOGI(TAG, "Ready");
 
-    WeavePlatform::SystemLayer.DispatchEvents();
+    SystemLayer.DispatchEvents();
 }
