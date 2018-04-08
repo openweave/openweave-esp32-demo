03bdf8bbc21fbc6d01b454d6ed5fc826c08ad739
Jay Logue <jaylogue@google.com>
2018-04-07 19:42:39 -0700

    Project Blue Sky : Enabled persistent storage if WiFi setting in the ESP32 test app.

--- main/weave-esp32-test.cpp
+++ main/weave-esp32-test.cpp
@@ -195,7 +195,7 @@ extern "C" void app_main()
         wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
         ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
 
-        ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+        ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_FLASH) );
 
         ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
 
